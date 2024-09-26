5c118359412bb8d85ec7075084c38b8aa01b561e
Keith McCoy <keith.mccoy@nisc.coop>
2024-09-26 14:13:57 -0500

    Initial setup of maven project from start.spring.io

---
+++ src/test/java/com/quasikafu/gaming/treasury/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quasikafu.gaming.treasury.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
