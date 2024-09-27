ef54af3c46e58db9f4ee964fdc8bcdcdef3002d1
Keith McCoy <keith.mccoy@nisc.coop>
2024-09-27 07:55:32 -0500

    Updated properties file and begin Login controller

---
+++ src/main/java/com/quasikafu/gaming/treasury/user/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.quasikafu.gaming.treasury.user.controllers;
+
+/**
+ * User: kmccoy
+ * Date: 9/26/2024
+ * Time: 4:26 PM
+ * *********************************************************************************************
+ * USR            DATE        JIRA#               COMMENTS
+ * xxxxxxxx       09/26/2024  BATCHJAVA-99999999  Created LoginController.
+ * *********************************************************************************************
+ */
+public class LoginController {
+}
